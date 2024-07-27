8738a4ce07d54625a690dce7414939e1d5f690c8
abitions <58674827+abitions@users.noreply.github.com>
2024-07-27 14:57:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oEy5Y68W2t_ANsVWFQ5mCByTf3e4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Y1ZQCqd4gnU11YccC3eorxUAHa-gaYgpWjdMlgTB8ZY',
+      useTemplateId: '1reiJwC5zIU9Zz1h66qn55rzq7XMUqv28aRmhoC__2w',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-21',
       festivals: [
